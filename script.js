0c95d44406546fcbb9bae8330fbf1f7fdc221bb1
Paras Adhikari <111726119+parasSinghAdhikari@users.noreply.github.com>
2025-05-01 13:40:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+let words = document.querySelectorAll(".word");
+
+words.forEach(word =>{
+    let letters = word.textContent.split("");
+    word.textContent="";
+    letters.forEach(letter=>{
+        let span = document.createElement("span");
+        span.textContent=letter; 
+        span.className="letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length -1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = ()=>{
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex===maxWordIndex ? words[0] : words[currentWordIndex +1];
+    
+    Array.from(currentWord.children).forEach((letter,i)=>{
+        setTimeout(()=>{
+            letter.className ="letter out";
+        },i*80);
+    });
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter,i)=>{
+         letter.className = "letter behind";
+         setTimeout(()=>{
+            letter.className ="letter in";
+        },340+i*80);
+    });
+    currentWordIndex = currentWordIndex===maxWordIndex  ? 0 : currentWordIndex +1;
+}
+changeText();
+setInterval(changeText,3000);
+
+// circle Skill/////////////////////////////////////////
+
+const circles =document.querySelectorAll(".circle")
+
+circles.forEach(element=>{
+    var dots = element.getAttribute("data-dots");
+    var marked =element.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360/dots;
+
+    for(let i =0 ;i<dots;i++){
+        points+=`<div class="points" style="--i:${i};--rot:${rotate}deg;"></div>`;
+    }
+    element.innerHTML =points;
+    
+    const pointsMarked = element.querySelectorAll(".points");
+
+    for (let i  = 0; i  < percent; i ++) {
+        pointsMarked[i].classList.add('marked');
+    }
+});
+
+//Projext Mixer//////////////////////////////////////
+var mixer = mixitup('.projects-box');
+
+// activeMenu //////////////////////////////////////////
+let menuLi = document.querySelectorAll(".header .navbar a");
+let section = document.querySelectorAll("section");
+
+function activeMenu(){
+    let len =section.length;
+    while(--len && window.scrollY+97<section[len].offsetTop){}
+    menuLi.forEach(sec=>sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+
+
+
+// menu icon active ////////////////////////////////////
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+
+// parllox ///////////////////
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
